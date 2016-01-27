7c515048a1305c063478d8271887621e3c360622
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-01-27 08:16:41 -0500

    Support pip

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@
 
 from distutils.core import setup
 
-setup(name='out_of_core_fft',
+setup(name='out-of-core-fft',
       description='Perform FFT on data set that does not fit into memory',
       author='Michael Boyle',
       author_email='mob22@cornell.edu',
-      version='1.0.0',
+      version='1.0',
       url='https://github.com/moble/out_of_core_fft',
       packages=['out_of_core_fft', ],
       package_dir={'out_of_core_fft': '.'},
